9c5b22af19f50c191a9ee250a2a91dde7686fb6f
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-05-30 13:31:13 +0100

    another extension

--- project/scripts/photography.js
+++ project/scripts/photography.js
@@ -261,7 +261,7 @@
 //     }
 // ];
 
-const membersFiles = "/project/data/members.json";
+const membersFiles = "/wdd131/project/data/members.json";
 async function getMembersFile() {
     const response = await fetch(membersFiles);
     const data = await response.json();
